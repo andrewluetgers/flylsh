34950dd4769b179d78021a71c82025748bea97f1
andrew luetgers <andrew.luetgers@gmail.com>
2017-12-11 23:12:36 -0600

    evaluation app updates

--- evaluate/server.js
+++ evaluate/server.js
@@ -8,9 +8,10 @@ let app = express(),
 app.use(express.static(path.join(__dirname, 'assets')));
 app.use("/data", express.static(path.join(__dirname, '../data')));
 
-console.log("serving", path.join(__dirname, 'assets'));
-
 // Listen for requests
 app.listen(port, function() {
 	console.log('Serving LSH evaluation app on port ' + port);
+	setTimeout(function() {
+		console.log('Serving LSH evaluation app on port ' + port);
+	}, 2000);
 });
